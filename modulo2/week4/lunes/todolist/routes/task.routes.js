d1f8be2ca04044e7ec802d58d5e4798794590223
Dylan <dylan@Dylans-MacBook-Pro.local>
2022-12-05 22:22:14 -0600

    edit and delete

--- modulo2/week4/lunes/todolist/routes/task.routes.js
+++ modulo2/week4/lunes/todolist/routes/task.routes.js
@@ -31,6 +31,59 @@ router.post("/create", async(req,res,next)=>{
     }
 })
 
+router.post("/:id/complete",async(req,res,next)=>{
+    const { id }  = req.params
+    const isComplete = true
+    try {                                   // (id,{datosAmodificar},{new:true})
+        const taskUpdated = await Task.findByIdAndUpdate(id,{isComplete},{new:true})
+        console.log("se actualizo?",taskUpdated)
+        res.redirect("/task")
+    } catch (error) {
+        next(error)
+    }
+})
+router.get("/:id/delete",async(req,res,next)=>{
+    const { id }  = req.params
+    try {
+        await Task.findByIdAndDelete(id)
+        res.redirect("/task")
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.get("/:id/edit",async(req,res,next)=>{
+    const {id} = req.params
+    try {
+
+        const taskEdit = await Task.findById(id)//<- es para llenar los campos con los datos a editar
+        const tasks = await Task.find()
+        res.render("task/taskHome",{ tasks, taskEdit, isEdit:true })
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post("/:id/edit",async(req,res,next)=>{
+    const {id} = req.params
+    //req.body = { ...datos }
+    const {isComplete} = req.body
+    try {
+        const taskUpdated2 = await Task.findByIdAndUpdate(id,{...req.body,
+                                                                isComplete: isComplete === "on" ?true :false 
+                                                            },{new:true} );
+        console.log("task 2", taskUpdated2)
+        res.redirect("/task")
+        //res.json(req.body)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
 
 //3)
 module.exports = router;
