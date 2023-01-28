186572a845e695f713aee7c843f7d5be486a4b57
Dylan <dylan@Dylans-MacBook-Pro.local>
2023-01-28 14:02:36 -0600

    Los temas

--- module3/semana1/sabado/state-demo/src/App.js
+++ module3/semana1/sabado/state-demo/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { EmptyMessage, FormGuest } from "./components";
 function App() {
 
   return (
     <div className="App">
 
+      <div className="section-guest">
+        <FormGuest/>
+        <div className="section-list">
+          <EmptyMessage/>
+        </div>
+      </div>
     </div>
   );
 }
